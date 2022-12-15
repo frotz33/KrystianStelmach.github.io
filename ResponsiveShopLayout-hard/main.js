da885bc16a8d4b729b51b9db62ab2193b666f19f
frotz33 <krystian.stelmach3@gmail.com>
2022-12-15 16:20:58 +0100

    fixed shop-layout script

--- ResponsiveShopLayout-hard/main.js
+++ ResponsiveShopLayout-hard/main.js
@@ -1,38 +1,11 @@
-// const shop = document.querySelector('.shop');
-// shop null
-// es modules
-const initOnClickHandler = () => {
-  // czasownik w funkcji
-  const highlightActiveProduct = (e) => {
-    const clickedElement = e.target;
-    const parentClickedElement = clickedElement.parentElement;
-    // if (!parentClickedElement) {
-    //   return
-    //   throw new Error()
-    //   console.log('nie ma takiego elelemtnu')
-    // }
+const navigation = document.querySelector('.navigation');
 
-    const parentHasClassShop =
-      clickedElement.parentElement.classList.contains('shop');
-    if (!clickedElement.classList.contains('shop__product')) {
-      return;
-    }
-
-    // if (clickedElement.parentElement == document.body) return;
-    else if (!parentHasClassShop) {
-      clickedElement.parentElement.classList.toggle('shop__product--active');
-    } else if (parentHasClassShop) {
-      clickedElement.classList.toggle('shop__product--active');
-    }
-
-    // if (element.classList.contains('shop__product')) {
-    //   element.classList.toggle('shop__product--active');
-    //   // do tego momentu działa ale dodaje klase dla calego .shop
-    // } else return;
-    // if (e.target.classList.contains('shop')) return;
-  };
-
-  const shop = document.querySelector('.shop');
-  shop.addEventListener('click', highlightActiveProduct);
+const scrollWebsite = () => {
+  if (window.scrollY >= 100) {
+    navigation.classList.add('navigation--active');
+    return;
+  }
+  navigation.classList.remove('navigation--active');
 };
-initOnClickHandler();
+
+window.addEventListener('scroll', scrollWebsite);
